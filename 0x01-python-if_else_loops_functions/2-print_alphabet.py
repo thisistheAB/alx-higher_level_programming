2e00c6ad520b333a0c2042eccdd4f3e03f14982f
thisistheAB <renbadyz@gmail.com>
2022-08-26 18:27:36 +0500

    2 - Print Alphabet - v3 - No quotes in print function

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,5 @@
 
 for i in range(97, 123):
     # print(f"{letter}", end='', sep='', flush=True)
-    print('{0}'.format(chr(i)), end='', sep='')
+    charHolder = "{}"
+    print(charHolder.format(chr(i)), end='', sep='')

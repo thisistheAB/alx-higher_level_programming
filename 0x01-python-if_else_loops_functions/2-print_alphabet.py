50ab9b38876170708ce6a2df23205dd97e9b983e
thisistheAB <renbadyz@gmail.com>
2022-08-26 14:21:18 +0500

    2 - Print Alphabet - v3 - String format fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
-    letter = chr(i)
-    print(f"{letter}", end='', sep='', flush=True)
+    # print(f"{letter}", end='', sep='', flush=True)
+    print('{0}'.format(chr(i)), end='', sep='')

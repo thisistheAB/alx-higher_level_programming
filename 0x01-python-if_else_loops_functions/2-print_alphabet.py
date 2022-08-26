9fcf2f3afb147bb03145a67f70de41037c245ca5
thisistheAB <renbadyz@gmail.com>
2022-08-26 18:33:32 +0500

    2 - Print Alphabet - v3 - Using :c

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
-    # print(f"{letter}", end='', sep='', flush=True)
-    charHolder = "{}"
-    print(charHolder.format(chr(i)), end='', sep='')
+    print('{:c}'.format(i), end='')

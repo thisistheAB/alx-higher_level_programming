0bddc7731bac9d7b50eb00c9c55a5f4a7ac689dc
thisistheAB <renbadyz@gmail.com>
2022-09-12 16:58:28 +0500

    8 - Uppercase - v4 No more than two print fix 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(iString):
     for i in iString:
         if 97 <= ord(i) <= 122:
             i = chr(ord(i) - 32)
-        print(i, end='')
+        print('{:s}'.format(i), end='')
 
     print('\n', end='')

ea390a6f3e35d9257808538e1c07328dafcd890d
thisistheAB <renbadyz@gmail.com>
2022-09-16 11:24:22 +0500

    0 - Add - v3 Import fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 
 
 def main():
     a = 1
     b = 2
 
-    addition = add_0.add(a, b)
+    addition = add(a, b)
     print('{:d} + {:d} = {:d}'.format(a, b, addition))
 
 

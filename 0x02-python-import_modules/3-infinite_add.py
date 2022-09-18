06baed57b811233e06ed7603410ef8c2809fad51
thisistheAB <renbadyz@gmail.com>
2022-09-19 00:20:56 +0500

    3 - Infinite Add - v1 No permission

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main():
+
+    numberArguments = len(sys.argv) - 1
+    counter = 0
+
+    if numberArguments > 0:
+        for i in range(0, numberArguments):
+            counter = counter + int(sys.argv[i+1])
+        print(counter)
+    else:
+        print('0')
+
+
+if __name__ == '__main__':
+    main()

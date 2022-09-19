2d893f9ef08a0a4086c38acdb2ca0a20db4811cb
thisistheAB <renbadyz@gmail.com>
2022-09-19 18:33:33 +0500

    4 - Hidden Discovery - v4 Python 3.8 fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
-import sys
 
-dir(hidden_4)
 
+def main():
+    for n in dir(hidden_4):
+        if n[1] != "_":
+            print(n)
+
+
+if __name__ == '__main__':
+    main()

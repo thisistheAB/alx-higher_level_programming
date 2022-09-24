4908f2a9351f2befdda0f9becf1aaf01698ed9b1
thisistheAB <renbadyz@gmail.com>
2022-09-24 21:33:48 +0500

    3 - Print Reversed List Integer - v3 Check list fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,8 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    length = len(my_list) - 1
-    for _ in my_list:
-        print('{:d}'.format(my_list[length]))
-        length -= 1
+    if my_list:
+        length = len(my_list) - 1
+        for _ in my_list:
+            print('{:d}'.format(my_list[length]))
+            length -= 1

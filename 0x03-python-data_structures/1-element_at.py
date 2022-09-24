23b5b401ee1d381511cd9c88ff5d3d7307e99703
thisistheAB <renbadyz@gmail.com>
2022-09-24 13:59:27 +0500

    1 - Element At - v1 No permission

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx > len(my_list) - 1:
+        return None
+
+    item = my_list[idx]
+    return item

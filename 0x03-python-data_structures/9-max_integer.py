7c7690d24845e5dbc28347561e55fe1337034357
thisistheAB <renbadyz@gmail.com>
2022-10-03 18:51:55 +0500

    9 - Max Integer - v4 Null case fix 2

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,10 +5,11 @@ def max_integer(my_list=[]):
 
     if not my_list:
         return None
+    else:
+        maxValue = 0
+        for currentValue in my_list:
+            if currentValue > maxValue:
+                maxValue = currentValue
 
-    maxValue = 0
-    for currentValue in my_list:
-        if currentValue > maxValue:
-            maxValue = currentValue
+        return maxValue
 
-    return maxValue

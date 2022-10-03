89981aae3eba07b55f1e5872ba504dbb68c68247
thisistheAB <renbadyz@gmail.com>
2022-10-03 17:03:11 +0500

    6 - Print Matrix Integer - v1 No permission

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    matrixLength = len(matrix)
+
+    for matrixCounter in range(0, matrixLength):
+        itemLength = len(matrix[matrixCounter])
+        for itemCounter in range(0, itemLength):
+            print('{:d}'.format(matrix[matrixCounter][itemCounter]), end=' ')
+        print('\n', end='')

6792a2ab8108afb752f3bb19de9878f4a6600efb
thisistheAB <renbadyz@gmail.com>
2022-10-03 17:31:29 +0500

    6 - Print Matrix Integer - v5 Output format fix 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-
-    matrixLength = len(matrix)
-
-    for matrixCounter in range(0, matrixLength):
-        itemLength = len(matrix[matrixCounter])
-        for itemCounter in range(0, itemLength):
-            print('{:d}'.format(matrix[matrixCounter][itemCounter]), end=' ')
-        if matrixCounter == matrixLength:
-            break
-        print('\n', end='')
+    for i in matrix:
+        for j in i:
+            if j == i[-1]:
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
+        print()

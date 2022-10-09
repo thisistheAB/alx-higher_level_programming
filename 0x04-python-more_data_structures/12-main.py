6473897a6ef59c1cf596c28bbbbbb429e41a283f
thisistheAB <renbadyz@gmail.com>
2022-10-10 02:42:34 +0500

    12 - Roman To Int - v2 Efficiency fix

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -18,5 +18,5 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "IX"
+roman_number = "000"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-
-    if not roman_string:
-        return 0
-
-    regularRomanDictionary = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    subtractiveRomanDictionary = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
-    subtractiveIndexes = dict()
-
-    inputLength = len(roman_string)
-    decimalNumber = 0
-
-    for subtractiveItemIndex in range(0, inputLength):
-        if subtractiveItemIndex == inputLength - 1:
-            break
-        subtractiveCheck = roman_string[subtractiveItemIndex] + roman_string[subtractiveItemIndex + 1]
-        if subtractiveCheck in subtractiveRomanDictionary:
-            subtractiveIndexes[subtractiveItemIndex] = subtractiveCheck
-
-    itemIndex = 0
-    while itemIndex < inputLength:
-        if itemIndex in subtractiveIndexes:
-            subtractiveNumber = subtractiveRomanDictionary[subtractiveIndexes[itemIndex]]
-            decimalNumber += subtractiveNumber
-            itemIndex += 1
-        elif roman_string[itemIndex] in regularRomanDictionary:
-            currentDecimalNumber = regularRomanDictionary[roman_string[itemIndex]]
-            decimalNumber += currentDecimalNumber
-        itemIndex += 1
-
-    return decimalNumber
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    integer = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_num[roman_string[i]] > roman_num[roman_string[i - 1]]:
+            integer += roman_num[roman_string[i]] - \
+                2 * roman_num[roman_string[i - 1]]
+        else:
+            integer += roman_num[roman_string[i]]
+    return integer

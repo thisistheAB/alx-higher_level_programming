c0dca6c6886d5d3daa68b909126486d36436c601
thisistheAB <renbadyz@gmail.com>
2022-10-10 03:04:26 +0500

    12 - Roman To Int - v5 Pycode 3

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 
 
 def roman_to_int(roman_string):
-    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_num = \
+        {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     integer = 0
 
     if roman_string is None or type(roman_string) is not str:
@@ -10,8 +11,10 @@ def roman_to_int(roman_string):
 
     for i in range(len(roman_string)):
 
-        if i > 0 and roman_num[roman_string[i]] > roman_num[roman_string[i - 1]]:
-            integer += roman_num[roman_string[i]] - (2 * roman_num[roman_string[i - 1]])
+        if i > 0 and \
+                roman_num[roman_string[i]] > roman_num[roman_string[i - 1]]:
+            integer += roman_num[roman_string[i]] - \
+                       (2 * roman_num[roman_string[i - 1]])
         else:
             integer += roman_num[roman_string[i]]
 
